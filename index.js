fa04c886127d75f1425c70a958352e6ecb2c4d3a
RyzenMu <muruga13in2022@gmail.com>
2024-10-06 12:49:15 +0530

    authorization

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const Datastore = require('nedb-promises')
 const bcrypt = require('bcryptjs');
+const config = require('./config');
 
 app.use(express.json()); 
 
 const users = Datastore.create('Users.db');
+const userRefreshTokens = Datastore.create('UserRefreshTokens.db')
 
 app.get('/', function(req, res){
     res.json({'greet': "Hello"})
@@ -13,7 +15,7 @@ app.get('/', function(req, res){
 
 app.post('/api/auth/register', async function(req, res){
     try{
-        const {email, password, name} = req.body;
+        const {email, password, name, role} = req.body;
         if(!name || !password ||!email){
             return res.status(422).json({message :"Please fill in all fields"})
         }
@@ -21,13 +23,18 @@ app.post('/api/auth/register', async function(req, res){
         const newUser = await users.insert({
             name,
             email,
-            password : hashedpassword
+            password : hashedpassword,
+            role : role ?? 'member'
         });
         return res.status(201).json({message:"User Registered Successfully"})
     }catch(err){
         return res.status(500).json({message: err.message})       
     }
-})
+});
+
+app.get('/api/auth/register', async function(req, res){
+    const refreshToken = jwt.sign({userId: user._id}, config.refreshTokenSecret, {subject: 'refreshToken', expiresIn: '1w'})
+} )
 
 app.get('api/auth/current',ensureAuthenticated, async (req, res) => {
     try {
@@ -42,6 +49,14 @@ app.get('api/auth/current',ensureAuthenticated, async (req, res) => {
     }
 }); 
 
+app.get('api/admin',ensureAuthenticated,authorize(['admin']), (req, res)=>{
+    return res.status(200).json({message: 'Only admin can access the route'})
+});
+
+app.get('api/moderator',ensureAuthenticated,authorize(['admin', 'moderator']), (req, res)=>{
+    return res.status(200).json({message: 'Only admins and moderators can access the route'})
+})
+
 async function ensureAuthenticated(req, res, next) {
     const accessToken = req.headers.authorization;
     if (!accessToken) {
@@ -53,6 +68,16 @@ async function ensureAuthenticated(req, res, next) {
         next();
     } catch (error) {
         return res.status(401).json({message : 'Access token invalid or expired'});
+    }
+}
+
+function authorize(){
+    return async function (req, res, next){
+        const user = await users.findOne({_id:req.user.id});
+        if(!user || !roles.includes(user.role)){
+            return res.status(403).json({mesage:'Access denied'})
+        }
+        next()
     }
 }
 
